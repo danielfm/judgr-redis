9a2f98233484218e0f970b50fb7f8eed4a0e40ab
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-04-26 11:57:21 -0300

    Added project URL.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject judgr/redis "0.1.0"
   :description "Redis support for Judgr"
+  :url "http://github.com/danielfm/judgr-redis"
   :dependencies [[judgr "0.1.1"]
                  [redis.clients/jedis "2.0.0"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
